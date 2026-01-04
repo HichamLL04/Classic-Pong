4602e7e65f6eaf8095fb3dd51ab3847d247170a3
Kaiser <hichamlaaouni52@gmail.com>
2026-01-04 22:55:19 +0100

    Rebote de la pelota
    Import de textmash tambien

---
+++ Assets/_Project/Scripts/Gameplay/BallMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] float velocidad = 5;
+    Rigidbody2D myrigidbody2D;
+    void Start()
+    {
+        myrigidbody2D = GetComponent<Rigidbody2D>();
+        myrigidbody2D.linearVelocity = new Vector2(velocidad, 0f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
