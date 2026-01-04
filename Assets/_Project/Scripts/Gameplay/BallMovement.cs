99d2e731f7aac18712c9c60703cb00af292b06f5
Kaiser <hichamlaaouni52@gmail.com>
2026-01-05 00:47:57 +0100

    Velocidad incremental
    Por cada rebote se suma 0.5 a la velocidad manteniendo lo demas con un maximo de 25

--- Assets/_Project/Scripts/Gameplay/BallMovement.cs
+++ Assets/_Project/Scripts/Gameplay/BallMovement.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    [SerializeField] float velocidad = 5;
+    [SerializeField] float velocidad = 10;
     [SerializeField] AudioClip gol;
     [SerializeField] AudioClip raqueta;
 
     [SerializeField] AudioClip pared;
+    [SerializeField] float incrementoVelocidad = 0.5f;
+    [SerializeField] float velocidadMaxima = 30f;
 
     Rigidbody2D myrigidbody2D;
     CircleCollider2D mycircleCollider2D;
@@ -21,24 +23,35 @@ public class BallMovement : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Paredes")))
         {
-           audioSource.PlayOneShot(pared);
-        }else if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Player")))
+            audioSource.PlayOneShot(pared);
+        }
+        else if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Player")))
         {
             audioSource.PlayOneShot(raqueta);
         }
+        if(velocidad <= velocidadMaxima)
+        {
+            velocidad += incrementoVelocidad;
+        }
+
+        Vector2 direccion = myrigidbody2D.linearVelocity.normalized;
+        myrigidbody2D.linearVelocity = direccion * velocidad;
+        Debug.Log(velocidad);
+       
+
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Paredes")))
         {
-           audioSource.PlayOneShot(gol);
+            audioSource.PlayOneShot(gol);
         }
     }
 }
