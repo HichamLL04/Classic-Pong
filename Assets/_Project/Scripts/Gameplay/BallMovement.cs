3ec1502969859e2c636a9ccc2f962b1284587191
Kaiser <hichamlaaouni52@gmail.com>
2026-01-05 00:06:41 +0100

    Sistema de sonido
    Dependiendo del trigger o collider, suena uno de los 3 audios. El audio pertenece a Atari

--- Assets/_Project/Scripts/Gameplay/BallMovement.cs
+++ Assets/_Project/Scripts/Gameplay/BallMovement.cs
@@ -3,15 +3,42 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     [SerializeField] float velocidad = 5;
+    [SerializeField] AudioClip gol;
+    [SerializeField] AudioClip raqueta;
+
+    [SerializeField] AudioClip pared;
+
     Rigidbody2D myrigidbody2D;
+    CircleCollider2D mycircleCollider2D;
+    AudioSource audioSource;
     void Start()
     {
         myrigidbody2D = GetComponent<Rigidbody2D>();
+        mycircleCollider2D = GetComponent<CircleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
         myrigidbody2D.linearVelocity = new Vector2(velocidad, 2f);
     }
 
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Paredes")))
+        {
+           audioSource.PlayOneShot(pared);
+        }else if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            audioSource.PlayOneShot(raqueta);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Paredes")))
+        {
+           audioSource.PlayOneShot(gol);
+        }
     }
 }
