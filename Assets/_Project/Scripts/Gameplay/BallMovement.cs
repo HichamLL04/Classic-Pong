3ad72a58a6fafdddd7c41c66ca87352d3fdb8348
Kaiser <hichamlaaouni52@gmail.com>
2026-01-05 01:51:55 +0100

    Saque y reinicio de juego
    Se randomiza quien saca al inicio con una inclinación además de al reiniciar que saque el lado contrario de quien marco el pinto.

--- Assets/_Project/Scripts/Gameplay/BallMovement.cs
+++ Assets/_Project/Scripts/Gameplay/BallMovement.cs
@@ -13,12 +13,17 @@ public class BallMovement : MonoBehaviour
     Rigidbody2D myrigidbody2D;
     CircleCollider2D mycircleCollider2D;
     AudioSource audioSource;
+
+    Vector2 lastDireccion;
+
+    float velocidadInicial;
     void Start()
     {
+        velocidadInicial = velocidad;
         myrigidbody2D = GetComponent<Rigidbody2D>();
         mycircleCollider2D = GetComponent<CircleCollider2D>();
         audioSource = GetComponent<AudioSource>();
-        myrigidbody2D.linearVelocity = new Vector2(velocidad, 2f);
+        StartGame();
     }
 
     void Update()
@@ -36,7 +41,7 @@ public class BallMovement : MonoBehaviour
         {
             audioSource.PlayOneShot(raqueta);
         }
-        if(velocidad <= velocidadMaxima)
+        if (velocidad <= velocidadMaxima)
         {
             velocidad += incrementoVelocidad;
         }
@@ -44,14 +49,46 @@ public class BallMovement : MonoBehaviour
         Vector2 direccion = myrigidbody2D.linearVelocity.normalized;
         myrigidbody2D.linearVelocity = direccion * velocidad;
         Debug.Log(velocidad);
-       
+
 
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (mycircleCollider2D.IsTouchingLayers(LayerMask.GetMask("Paredes")))
+        if (collision.CompareTag("Azul"))
         {
             audioSource.PlayOneShot(gol);
+            RestartGame(Vector2.right);
         }
+        else if (collision.CompareTag("Naranja"))
+        {
+            audioSource.PlayOneShot(gol);
+            RestartGame(Vector2.left);
+        }
+    }
+
+    void StartGame()
+    {
+        float inclinacion = Random.Range(-0.4f, 0.4f);
+        Vector2 direcion = Random.value < 0.5f ? Vector2.left : Vector2.right;
+        direcion = new Vector2(direcion.x, inclinacion).normalized;
+        myrigidbody2D.linearVelocity = direcion * velocidad;
+    }
+
+
+    void RestartGame(Vector2 direccion)
+    {
+        velocidad = velocidadInicial;
+        myrigidbody2D.linearVelocity = Vector2.zero;
+        transform.position = Vector3.zero;
+
+
+        lastDireccion = direccion;
+        Invoke(nameof(MoveBall), 1f);
+    }
+
+    void MoveBall()
+    {
+        Vector2 dir = new Vector2(-lastDireccion.x, Random.Range(-0.4f, 0.4f)).normalized;
+        myrigidbody2D.linearVelocity = dir * velocidad;
     }
 }
---
+++ Assets/_Project/Scripts/Gameplay/CountPoints.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CountPoints : MonoBehaviour
+{
+    [SerializeField] GameObject pelota;
+    [SerializeField] TextMeshPro socreAzul;
+    [SerializeField] TextMeshPro socreNaranja;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
